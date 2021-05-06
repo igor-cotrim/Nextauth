bec662ca34805534db9bf394a13b591751d1b18b
igor-cotrim <igorxuxicotrim@gmail.com>
2021-05-05 21:35:28 -0300

    criou hook de permissão para usuario

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,9 @@
 import { GetServerSideProps } from "next"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from '../utils/withSSRAuth'
@@ -16,7 +18,13 @@ export default function dashboard() {
   }, [])
 
   return (
+    <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
+    </>
   )
 }
 
