1b6f10bed353cf7597d03500f4c5fb8b96412d13
igor-cotrim <igorxuxicotrim@gmail.com>
2021-05-05 22:10:55 -0300

    validando permissões (Server) e Broadcast de logout

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -9,7 +9,7 @@ import { api } from "../services/apiClient"
 import { withSSRAuth } from '../utils/withSSRAuth'
 
 export default function dashboard() {
-  const {user} = useContext(AuthContext)
+  const {user, signOut} = useContext(AuthContext)
 
   useEffect(() => {
     api.get('/me')
@@ -20,6 +20,8 @@ export default function dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+
+      <button onClick={signOut}>Sign out</button>
 
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
